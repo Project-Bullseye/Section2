115e555b677c33036d1ae857eebfa22e261337d9
Gaven Rudiak <gavenrudiak@outlook.com>
2020-09-29 08:31:07 -0400

    Add project files.

---
+++ Section 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Section2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Check();
+        }
+
+        static void Check()
+        {
+            Section2.ScoreKeeper();
+        }
+    }
+}
---
+++ Section 2/Section2/section2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+
+namespace Section2
+{
+    class Section2
+    {
+        public static void ScoreKeeper()
+        {
+            FolderCreateCheck();
+        }
+
+        //checking if the folder and file and already there and if not creating them
+        public static void FolderCreateCheck()
+        {
+            string path = "C:\\Users\\Public\\Documents\\myData.txt";
+
+            if (!File.Exists(path))
+            {
+                // Create the file if not already made.
+                using (FileStream fs = File.Create(path))
+                {
+                    //puts text in it
+                    Byte[] info =
+                   new UTF8Encoding(true).GetBytes("This is where we will keep the score");
+
+                    fs.Write(info, 0, info.Length);
+                }
+            }
+            else
+            {
+                //writes in file for test
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    sw.WriteLine("This is where we will keep the score 2");
+                }
+            }
+        }
+    }
+}
